e4d1f6718032b5a9f3cae930d8cc86797f403c2b
sidou <sidalimouhous9@gmail.com>
2026-04-22 23:40:24 +0200

    fixed bug conductor

--- services/conductor-service/src/Entity/Conductor.php
+++ services/conductor-service/src/Entity/Conductor.php
@@ -2,37 +2,45 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ConductorRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ConductorRepository::class)]
 #[ORM\Table(name: 'conducteurs')]
 #[ORM\HasLifecycleCallbacks]
+#[ApiResource]
 class Conductor
 {
     #[ORM\Id]
     #[ORM\Column(type: 'string', length: 36, unique: true)]
     private ?string $id = null;
 
-    #[ORM\Column(type: 'string', length: 50)]
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank]
     private ?string $nom = null;
 
-    #[ORM\Column(type: 'string', length: 50)]
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank]
     private ?string $prenom = null;
 
-    #[ORM\Column(type: 'string', length: 100, unique: true)]
+    #[ORM\Column(length: 100, unique: true)]
+    #[Assert\NotBlank]
+    #[Assert\Email]
     private ?string $email = null;
 
-    #[ORM\Column(type: 'string', length: 20, nullable: true)]
+    #[ORM\Column(length: 20, nullable: true)]
     private ?string $telephone = null;
 
-    #[ORM\Column(type: 'string', length: 30, unique: true)]
+    #[ORM\Column(length: 30, unique: true)]
+    #[Assert\NotBlank]
     private ?string $numeroPermis = null;
 
     #[ORM\Column(type: 'date_immutable')]
     private ?\DateTimeImmutable $dateExpirationPermis = null;
 
-    #[ORM\Column(type: 'string', length: 20)]
+    #[ORM\Column(length: 20)]
     private string $statut = 'actif';
 
     #[ORM\Column(type: 'datetime_immutable')]
@@ -43,15 +51,17 @@ class Conductor
 
     public function __construct()
     {
-        $this->id = self::generateUuidV4();
+        $data = random_bytes(16);
+        $data[6] = chr((ord($data[6]) & 0x0f) | 0x40);
+        $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
+        $this->id = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
     #[ORM\PrePersist]
     public function onPrePersist(): void
     {
-        $now = new \DateTimeImmutable();
-        $this->createdAt = $this->createdAt ?? $now;
-        $this->updatedAt = $this->updatedAt ?? $now;
+        $this->createdAt = new \DateTimeImmutable();
+        $this->updatedAt = new \DateTimeImmutable();
     }
 
     #[ORM\PreUpdate]
@@ -60,111 +70,29 @@ class Conductor
         $this->updatedAt = new \DateTimeImmutable();
     }
 
-    private static function generateUuidV4(): string
-    {
-        $data = random_bytes(16);
-        $data[6] = chr((ord($data[6]) & 0x0f) | 0x40);
-        $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
+    public function getId(): ?string { return $this->id; }
 
-        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-    }
+    public function getNom(): ?string { return $this->nom; }
+    public function setNom(string $nom): self { $this->nom = $nom; return $this; }
 
-    public function getId(): ?string
-    {
-        return $this->id;
-    }
+    public function getPrenom(): ?string { return $this->prenom; }
+    public function setPrenom(string $prenom): self { $this->prenom = $prenom; return $this; }
 
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
+    public function getEmail(): ?string { return $this->email; }
+    public function setEmail(string $email): self { $this->email = $email; return $this; }
 
-    public function setNom(string $nom): self
-    {
-        $this->nom = $nom;
+    public function getTelephone(): ?string { return $this->telephone; }
+    public function setTelephone(?string $telephone): self { $this->telephone = $telephone; return $this; }
 
-        return $this;
-    }
+    public function getNumeroPermis(): ?string { return $this->numeroPermis; }
+    public function setNumeroPermis(string $numeroPermis): self { $this->numeroPermis = $numeroPermis; return $this; }
 
-    public function getPrenom(): ?string
-    {
-        return $this->prenom;
-    }
+    public function getDateExpirationPermis(): ?\DateTimeImmutable { return $this->dateExpirationPermis; }
+    public function setDateExpirationPermis(\DateTimeImmutable $date): self { $this->dateExpirationPermis = $date; return $this; }
 
-    public function setPrenom(string $prenom): self
-    {
-        $this->prenom = $prenom;
+    public function getStatut(): string { return $this->statut; }
+    public function setStatut(string $statut): self { $this->statut = $statut; return $this; }
 
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getTelephone(): ?string
-    {
-        return $this->telephone;
-    }
-
-    public function setTelephone(?string $telephone): self
-    {
-        $this->telephone = $telephone;
-
-        return $this;
-    }
-
-    public function getNumeroPermis(): ?string
-    {
-        return $this->numeroPermis;
-    }
-
-    public function setNumeroPermis(string $numeroPermis): self
-    {
-        $this->numeroPermis = $numeroPermis;
-
-        return $this;
-    }
-
-    public function getDateExpirationPermis(): ?\DateTimeImmutable
-    {
-        return $this->dateExpirationPermis;
-    }
-
-    public function setDateExpirationPermis(\DateTimeImmutable $dateExpirationPermis): self
-    {
-        $this->dateExpirationPermis = $dateExpirationPermis;
-
-        return $this;
-    }
-
-    public function getStatut(): string
-    {
-        return $this->statut;
-    }
-
-    public function setStatut(string $statut): self
-    {
-        $this->statut = $statut;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updatedAt;
-    }
+    public function getCreatedAt(): ?\DateTimeImmutable { return $this->createdAt; }
+    public function getUpdatedAt(): ?\DateTimeImmutable { return $this->updatedAt; }
 }
