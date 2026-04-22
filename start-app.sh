7165e516cb268d192f005e45bc5052c87de329d3
Younes khx <younes.khadraoui.pro@gmail.com>
2026-04-22 21:27:34 +0200

    chore: updated run script

--- start-app.sh
+++ start-app.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Opening bridges to FleetManager Application..."
-kubectl port-forward svc/api-gateway 8080:8080 -n fleet-manager &
-kubectl port-forward svc/keycloak-service 8081:8080 -n fleet-manager &
+kubectl port-forward svc/api-gateway 8081:8080 -n fleet-manager &
+kubectl port-forward svc/keycloak-service 8080:8080 -n fleet-manager &
 
 echo "------------------------------------------------"
-echo "Apollo Explorer: http://localhost:8080"
-echo "Keycloak Admin:   http://localhost:8081"
+echo "Apollo Explorer: http://localhost:8081"
+echo "Keycloak Admin:   http://localhost:8080"
 echo "------------------------------------------------"
 echo "Press [Ctrl+C] to stop the app bridge."
-wait
+wait·
