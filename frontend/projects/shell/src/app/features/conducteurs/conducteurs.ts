10f85296a882697d892d42341ff1602df3c44b52
sidou <sidalimouhous9@gmail.com>
2026-04-22 17:08:48 +0200

    feat:  keyloack  auth

---
+++ frontend/projects/shell/src/app/features/conducteurs/conducteurs.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conducteurs',
+  imports: [],
+  templateUrl: './conducteurs.html',
+  styleUrl: './conducteurs.scss',
+})
+export class Conducteurs {}
